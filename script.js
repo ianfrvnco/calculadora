4900fdf7f59565ab7e6709a0281c2511210ac37c
Ian <ianfranco2@gmail.com>
2025-04-16 22:10:33 -0300

    atualização

--- script.js
+++ script.js
@@ -37,3 +37,11 @@ function potencia() {
 
   document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
 }
+
+function resto() {
+  const v1 = Number(document.getElementById("valor1").value);
+  const v2 = Number(document.getElementById("valor2").value);
+  const resultado = v1 % v2;
+
+  document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
